ae14e1d91e86b0cbb0d4c3a12d483e74741b453a
luci <lrodriguesg12@educarex.es>
2025-04-07 18:46:58 +0200

    Cambio en el h1 de la página

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,7 +12,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div>
-          <h1 className="text-9xl text-pink-500">Esto es mi página web</h1>
+          <h1 className="text-9xl text-pink-500">Esto es mi nueva página web</h1>
           <h2 className="text-6xl hover:text-yellow-300">Hola</h2>
           <a href="/perfil">
             Navegar a mi perfil
