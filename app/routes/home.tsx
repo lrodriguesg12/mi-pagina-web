afb94417d8327d9f143a4f4476706c12d56ea8a2
lrodrigues12 <lrodriguesg12@educarex.es>
2025-04-24 20:06:32 +0200

    Traslado a Windows

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,6 +13,7 @@ export default function Home() {
   return (
     <div>
           <h1 className="text-9xl text-pink-500">Esto es mi nueva página web</h1>
+          <h2 className="text-7xl text-blue-500"> En la rama MAIN</h2>
           <h2 className="text-6xl hover:text-yellow-300">Hola</h2>
           <a href="/perfil">
             Navegar a mi perfil
