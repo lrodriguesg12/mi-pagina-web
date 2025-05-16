05dd8994fc415450ee97420b1775bf22aa02e6ed
lrodrigues12 <lrodriguesg12@educarex.es>
2025-05-17 01:02:23 +0200

    .

--- app/components/Spotify.tsx
+++ app/components/Spotify.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 const Spotify = () => {
   return (
-    <div className="w-full max-w-lg">
-      <div>
+    <div className="w-full max-w-lg text-center">
         <iframe
           style={{ borderRadius: "12px" }}
-          src="https://open.spotify.com/embed/track/7cv28LXcjAC3GsXbUvXKbX?utm_source=generator"
+          src="https://open.spotify.com/embed/track/63BcfK6YAzJYeISaTPr6IO?utm_source=generator"
           width="100%"
-          height="152"
+          height="270"
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-          className="w-full"
+          
         ></iframe>
-      </div>
     </div>
   );
 };
--- app/routes/info.tsx
+++ app/routes/info.tsx
@@ -10,10 +10,10 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Info() {
   return (
-    <div className="bg-gray-50 rounded-xl m-30 pb-20 pt-20">
+    <div className="bg-gray-50 rounded-xl m-30 pb-20 pt-16 justify-items-center">
       <h1 className="text-center text-3xl font-bold pt-8 lg:pt-0 font-['Cal_Sans'] bg-gradient-to-r from-orange-400 to-pink-600 bg-clip-text text-transparent">HOLAAAAAAAAAAA</h1>
-						<div className="mx-auto w-4/5 pt-3 border-b-2 border-pink-500 opacity-25"></div>
-						<p className="pt-8 text-md font-['Poppins'] text-justify m-10 mt-0">Soy un cruce de Border Collie con Beagle. Nací en Valdehúncar hace 5 años, con un montón de hermanos, todos distintos entre nosotros. Tuve la suerte de que mi dueña me adoptó junto con mi hermana bruja, que se fue con mi tía. Me encanta comer, a lo mejor por eso dicen que estoy gordo... Y morder los muebles jeje. Me paso casi todo el día durmiendo en el sillón agustito, menos si pasa alguien por debajo de mi ventana, lo cual es una amenaza enorme y tengo que ladrar para que se enteren todos!</p>
+			<div className="mx-auto w-4/5 pt-3 border-b-2 border-pink-500 opacity-25"></div>
+			<p className="pt-8 text-md font-['Poppins'] text-justify m-10 mt-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aut fuga, sint nihil tempore officia placeat et? Voluptatum reiciendis eius dolorum, ullam aspernatur doloremque repellat? Tempora magni culpa a voluptatibus.</p>
       <Calendar/>
     </div>
   );
